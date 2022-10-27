3612d41bb6d3b82df8c2e05995b18ac987215393
Kristian-Buriasco <114618294+Kristian-Buriasco@users.noreply.github.com>
2022-10-27 16:39:25 +0200

    Update Keywords.java

--- src/main/java/co/aurasphere/gomorrasql/Keywords.java
+++ src/main/java/co/aurasphere/gomorrasql/Keywords.java
@@ -11,25 +11,24 @@ import java.util.List;
  */
 public class Keywords {
 
-	public final static String SELECT_KEYWORD = "ripigliammo";
-	public final static String UPDATE_KEYWORD = "rifacimm";
-	public final static String[] INSERT_KEYWORDS = { "nzipp", "'ngoppa" };
-	public static final String[] DELETE_KEYWORDS = { "facimm", "na'", "strage" };
-	public static final String[] JOIN_KEYWORDS = { "pesc", "e", "pesc" };
-	public static final String[] FROM_KEYWORDS = { "mmiez", "'a" };
-	public final static String[] ASTERISK_KEYWORDS = { "tutto", "chillo", "ch'era", "'o", "nuostro" };
-	public final static String WHERE_KEYWORD = "arò";
-	public final static String[] BEGIN_TRANSACTION_KEYWORDS = { "ua", "uagliò" };
-	public final static String[] COMMIT_KEYWORDS = { "iamme", "bello", "ia'" };
-	public final static String ROLLBACK_KEYWORD = "sfaccimm";
+	public final static String SELECT_KEYWORD = "ciapa";
+	public final static String UPDATE_KEYWORD = "rifuma";
+	public final static String[] INSERT_KEYWORDS = { "campa","'ndrinta"};
+	public static final String[] DELETE_KEYWORDS = { "fuma", "'ngran'", "casin" };
+	public static final String JOIN_KEYWORD = "taca";
+	public static final String[] FROM_KEYWORDS = { "da", "'ndrinta"};
+	public final static String WHERE_KEYWORD = "'ndua";
+	public final static String BEGIN_TRANSACTION_KEYWORD = "'nduma";
+	public final static String COMMIT_KEYWORD = "scuta";
+	public final static String[] ROLLBACK_KEYWORDS= { "turnuma", "'ndarÃ©"};
 	public final static String AND_KEYWORD = "e";
 	public final static String OR_KEYWORD = "o";
-	public final static String NULL_KEYWORD = "nisciun";
-	public final static String IS_KEYWORD = "è";
+	public final static String NULL_KEYWORD = "gnun";
+	public final static String IS_KEYWORD = "l'Ã¨";
 	public final static String VALUES_KEYWORD = "chist";
-	public final static String[] IS_NOT_KEYWORDS = { "nun", "è" };
-	public final static String SET_KEYWORD = "accunza";
+	public final static String[] IS_NOT_KEYWORDS = { "l'Ã¨", "nen" };
+	public final static String SET_KEYWORD = "buta";
 	public final static List<String> WHERE_OPERATORS = Arrays.asList(">", "<", "=", "!=", "<>", ">=", "<=",
 			Keywords.IS_KEYWORD, Keywords.IS_NOT_KEYWORDS[0]);
-	public final static String SET_EQUAL_KEYWORD = "accussì";
+	public final static String SET_EQUAL_KEYWORD = "parÃ¨j";
 }
